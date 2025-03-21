7e2cc3f07993da8be6e8bce15c259642e2ec995c
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-03-21 09:18:42 +0000

    soluzione

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,45 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
 
 // Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    
+    private static Scanner input = new Scanner(System.in);
+    private static Scanner infile;
+    private static FileWriter outfile;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void main(String[] args) throws Exception {
+        String inputFileName, outFileName, riga;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.println("nome del file di input: ");
+        inputFileName = input.nextLine();
+        System.out.println("nome del file di output: ");
+        outFileName = input.nextLine();
+        infile = new Scanner(new FileReader(inputFileName));
+        outfile = new FileWriter(new File(outFileName));
+        do {
+            riga = infile.nextLine();
+            riga = inverti(riga);
+            outfile.write(riga + "\n");
+        } while ( infile.hasNextLine() );
+        infile.close();
+        outfile.close();
+    }
+    
+    public static String inverti(String riga) {
+        int n, i;
+        String agir;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        agir = "";
+        n = riga.length();
+        for (i = n - 1; i >= 0; i--) {
+            agir = agir + riga.charAt(i);
+        }
+        
+        return agir;
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
